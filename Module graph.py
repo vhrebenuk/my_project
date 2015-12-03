cebb13a781c6cbfb4dc9cb0cc9d6ec50d270be76
Виктор Гребенюк < >
2015-12-03 23:55:27 +0200

    Module for clever house

--- Module graph.py
+++ Module graph.py
@@ -2,14 +2,26 @@ import socket
 from os import path
 import matplotlib.pyplot as plt
 from pyparsing import Word, Literal, nums
+import ftplib
 
-def listen_serv():
+def ftp_send():   #Отправка графика на сервер
+    host = "ftp.ex.ru"
+    ftp_user = "root"
+    ftp_password = "python"
+    filename = "picture.png"
+ 
+    con = ftplib.FTP(host, ftp_user, ftp_password)
+    f = open(filename, "rb")
+    send = con.storbinary("STOR "+ filename, f)
+
+    con.close
+
+def listen_serv():       
     conn, addr = sock.accept()
     print("Hello new user: ", addr[0])
     while True:
         data = conn.recv(1024)
         udata = data.decode("utf-8")
-        print (udata)
         menu_for_graph(udata)
         if not data:
             break
@@ -18,7 +30,6 @@ def listen_serv():
 def menu_for_graph(udata):
     if udata == "g":
         last_line = pars_data()
-        #list_y=[]
         makegraph(last_line)
         listen_serv()
     elif udata == "s":
@@ -33,19 +44,21 @@ def pars_data():
         item += item
     temperatura = (Word(nums + '+.') | Word(nums + '-.'))
     comma = (Literal(",") | Literal(";")).suppress()
-    full_name = Word(nums + '.').suppress() + Word(nums + ':').suppress() + (temperatura + comma)*5
-    vremenno= full_name.parseString(item)
+    tem = (temperatura + comma)*5
+    full_name_temperatura = Word(nums + '.').suppress() + Word(nums + ':').suppress() + tem # temperature
+    vremenno_temp= full_name_temperatura.parseString(item)                                  # temperature
+    full_name_date = Word(nums + '.')                                   # date
+    vremenno_date = full_name_date.parseString(item)                    # date
+    data_date = str(vremenno_date)
     data_temperatura = []
-    for item in vremenno:
+    for item in vremenno_temp:
         item = float(item)
         data_temperatura.append(item)
-    print(data_temperatura)
     f.close
     return data_temperatura
 
-def makegraph(list_y):
-    print(list_y)
-    Y_10 = list_y
+def makegraph(data_temperatura):
+    Y_10 = data_temperatura
     X = [1, 2, 3, 4, 5]
     line = plt.plot(X, Y_10, 'go:')
     plt.axis([1, 5, -40, 40])
