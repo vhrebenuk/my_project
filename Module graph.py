60fdb0aeaf17282956321e1e8ea710f1acc7de70
Виктор Гребенюк < >
2015-12-04 00:53:21 +0200

    Module for clever house

--- Module graph.py
+++ Module graph.py
@@ -4,38 +4,21 @@ import matplotlib.pyplot as plt
 from pyparsing import Word, Literal, nums
 import ftplib
 
-def ftp_send():   #Отправка графика на сервер
-    host = "ftp.ex.ru"
-    ftp_user = "root"
-    ftp_password = "python"
-    filename = "picture.png"
- 
-    con = ftplib.FTP(host, ftp_user, ftp_password)
-    f = open(filename, "rb")
-    send = con.storbinary("STOR "+ filename, f)
+def progress():    #Процесс отправка графика на сервер
+    def callback(block):
+        callback.uploaded += len(block)
+        print('Uploaded %d bytes' % callback.uploaded)
+    callback.uploaded = 0
+    return callback
 
-    con.close
-
-def listen_serv():       
-    conn, addr = sock.accept()
-    print("Hello new user: ", addr[0])
-    while True:
-        data = conn.recv(1024)
-        udata = data.decode("utf-8")
-        menu_for_graph(udata)
-        if not data:
-            break
-    conn.close()
-
-def menu_for_graph(udata):
+def menu_for_graph(udata):     #Вариативное меню для программы
     if udata == "g":
         last_line = pars_data()
         makegraph(last_line)
-        listen_serv()
     elif udata == "s":
         print("settings")
 
-def pars_data():
+def pars_data():            #Парсинг файла с данными
     f = open("test.txt" ,'r')
     all_lines = f.readlines()
     last_line = []
@@ -45,19 +28,19 @@ def pars_data():
     temperatura = (Word(nums + '+.') | Word(nums + '-.'))
     comma = (Literal(",") | Literal(";")).suppress()
     tem = (temperatura + comma)*5
-    full_name_temperatura = Word(nums + '.').suppress() + Word(nums + ':').suppress() + tem # temperature
-    vremenno_temp= full_name_temperatura.parseString(item)                                  # temperature
-    full_name_date = Word(nums + '.')                                   # date
-    vremenno_date = full_name_date.parseString(item)                    # date
-    data_date = str(vremenno_date)
-    data_temperatura = []
-    for item in vremenno_temp:
-        item = float(item)
-        data_temperatura.append(item)
+    full_name_temperatura = Word(nums + '.').suppress() + Word(nums + ':').suppress() + tem # парсинг температуры
+    vremenno_temp= full_name_temperatura.parseString(item)                                  # парсинг температуры
+    full_name_date = Word(nums + '.')                                   # парсинг даты
+    vremenno_date = full_name_date.parseString(item)                    # парсинг даты
+    data_date = str(vremenno_date)                                      # парсинг даты
+    data_temperatura = []                                                                   # парсинг температуры
+    for item in vremenno_temp:                                                              # парсинг температуры
+        item = float(item)                                                                  # парсинг температуры
+        data_temperatura.append(item)                                                       # парсинг температуры
     f.close
     return data_temperatura
 
-def makegraph(data_temperatura):
+def makegraph(data_temperatura):                                        # делаем и сохраняем график 
     Y_10 = data_temperatura
     X = [1, 2, 3, 4, 5]
     line = plt.plot(X, Y_10, 'go:')
@@ -66,8 +49,28 @@ def makegraph(data_temperatura):
     plt.ylabel(u'Temperatura')
     plt.grid()
     print("График построен")
-    plt.savefig("graphic.png", dpi = 250)        
+    plt.savefig("graphic.png", dpi = 250)
+    ftp_send()
 
+def ftp_send():                                         #Отправка графика на сервер
+    host = "node0.net2ftp.ru"
+    ftp_user = "grebenuk.viktor@gmail.com"
+    ftp_password = "8b0aab38"
+    filename = 'graphic.png'
+    
+    con = ftplib.FTP(host, ftp_user, ftp_password)
+    con.storbinary("STOR "+ filename, open(filename, "rb"), 1024, progress())   # Передаем файл на сервер
+
+def listen_serv():                                      #Слушаем на предмет команд контроллер умного дома
+    conn, addr = sock.accept()
+    print("Hello new user: ", addr[0])
+    while True:
+        data = conn.recv(1024)
+        udata = data.decode("utf-8")
+        menu_for_graph(udata)
+        if not data:
+            listen_serv()
+    conn.close()
     
 sock = socket.socket ()
 sock.bind(("", 13330))
